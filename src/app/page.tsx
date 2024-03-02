5262364ef7ec0f90edeb642ea630679b42927614
AngeloVitor <angelovitor2010@gmail.com>
2024-03-01 23:26:25 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function page() {
+  return (
+    <div className="container">
+      <nav>
+        <ul>
+          <li>
+            <strong>ASPECTA</strong>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <a href="#" className="contrast">
+              Sobre
+            </a>
+          </li>
+          <li>
+            <a href="#" className="contrast">
+              Preço
+            </a>
+          </li>
+          <li>
+            <a href="#" className="contrast">
+              Contato
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default page;
